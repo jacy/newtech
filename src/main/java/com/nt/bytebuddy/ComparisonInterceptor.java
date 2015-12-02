045065bffcb7c639068894750076a16b73ffb018
jacy <jacy@jc.local>
2015-12-02 18:31:45 +0800

    add pom

--- src/main/java/com/nt/bytebuddy/ComparisonInterceptor.java
+++
@@ -1,8 +0,0 @@
-package com.nt.bytebuddy;
-
-public class ComparisonInterceptor {
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public int compare(Object first, Object second) {
-		return ((Comparable) first).compareTo(second);
-	}
-}

1c4b8d50e3dfe28c8fbce35c3c15aaf3c52b90db
jacy <johnsoncr.hong@gmail.com>
2018-02-07 17:47:50 +0800

    add akka helloworld

---
+++ src/main/java/com/nt/akka/Test.java
@@ -0,0 +1,35 @@
+package com.nt.akka;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+public class Test extends AbstractActor {
+
+	@Override
+	public Receive createReceive() {
+		return receiveBuilder().match(String.class, s -> System.out.println(">>>> " + s)).matchAny(any -> System.out.println(">>> unknown msg")).build();
+	}
+
+	public static void main(String[] args) {
+		final ActorSystem actorSystem = ActorSystem.create("test");
+		Props props = Props.create(Test.class);
+		ActorRef actor = actorSystem.actorOf(props, "1");
+		ActorRef actor2 = actorSystem.actorOf(props, "2");
+		actor.tell("hello world", actor2);
+
+		new Thread(() -> {
+			try {
+				Thread.sleep(5000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			actorSystem.actorSelection("akka://test/user/1").tell("send from actor select", null);
+
+		}).start();
+		System.out.println(actor.path());
+
+	}
+
+}
